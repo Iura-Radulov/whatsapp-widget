b873ec5e1c8fa22ee4af0abc2378e5a9c77e3304
Iura-Radulov <91605430+Iura-Radulov@users.noreply.github.com>
2022-08-19 08:36:01 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
@@ -8,12 +8,12 @@ const root = ReactDOM.createRoot(document.querySelector('.whatsappwidget'));
 const widget = document.querySelector('.whatsappwidget');
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='/whatsapp-widget'>
-      <App
-        show={widget.dataset.status}
-        chat={widget.dataset.chat}
-        clientid={widget.dataset.clientid}
-      />
-    </BrowserRouter>
+    {/* <BrowserRouter basename='/whatsapp-widget'> */}
+    <App
+      show={widget.dataset.status}
+      chat={widget.dataset.chat}
+      clientid={widget.dataset.clientid}
+    />
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
